4c6ca67bba33c7c99d2459077bab6566ad3d46ef
Abhishek <56558684+abhisheksharma1123@users.noreply.github.com>
2022-01-24 09:54:17 +0000

    redux-compose

--- test.mjs
+++ test.mjs
@@ -1,5 +1,9 @@
+import { compose } from "redux";
+
 const makeLouder = (string) => string.toUpperCase();
 const repeatThreeTimes = (string) => string.repeat(3);
 const embolden = (string) => string.bold();
 const result = embolden(repeatThreeTimes(makeLouder("hello")));
-console.log(result);
+// console.log(result);
+
+console.log(compose(embolden, repeatThreeTimes, makeLouder)("hello"));
