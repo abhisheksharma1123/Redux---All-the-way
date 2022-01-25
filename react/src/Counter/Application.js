24166ef411a35ade86280a433ecccbb1f9f03018
Abhishek <56558684+abhisheksharma1123@users.noreply.github.com>
2022-01-25 01:21:17 +0000

    final-counter-app-build

--- react/src/Counter/Application.js
+++ react/src/Counter/Application.js
@@ -9,7 +9,13 @@ export const Application = () => {
   const dispatch = useDispatch();
   return (
     <main>
-      <p>{count}</p>
+      <p>
+        This is the counter application built using react and redux. The entry
+        file is main.js which is present in Counter folder and also Provider
+        Component from 'react-redux' is present in this file. So main.js is the
+        root of this application and main logic resides in Application.js
+      </p>
+      <h1>{count}</h1>
       <section>
         <button onClick={() => dispatch(increment())}>Increment</button>
         <button onClick={() => dispatch(set(0))}>Reset</button>
@@ -20,6 +26,7 @@ export const Application = () => {
           onSubmit={(event) => {
             event.preventDefault();
             dispatch(set(inputRef.current.value));
+            inputRef.current.value = 0;
           }}
         >
           <label htmlFor="set-to">Set Count: </label>
